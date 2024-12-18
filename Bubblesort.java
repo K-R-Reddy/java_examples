bcb645b91f8fcf736c4360bf0ff04f6cfabadccb
Karri Rajeswara Reddy <153280024+K-R-Reddy@users.noreply.github.com>
2024-12-18 16:07:47 +0530

    lab 0

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 
 public class Bubblesort {
+ static void Bsort{
+ int temp;
+ boolean swap=false;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int k=0;k<n-i-1;k++)
+            {
+                if(a[k]>a[k+1])
+                {
+                    temp=a[k];
+                    a[k]=a[k+1];
+                    a[k+1]=temp;
+                    swap=true;
+                }
+
+            }
+            if (swap==false){
+                 break;
+            }
+        }
+ }
  public static void main(String[] args) {
     Scanner s=new Scanner(System.in);
         System.out.println("Enter how many Elements in Array : ");
@@ -16,20 +37,7 @@ public class Bubblesort {
         for (int j : a) {
             System.out.print(j+" ");
         }
-        int temp;
-        for(int i=0;i<n-1;i++)
-        {
-            for(int k=0;k<n-i-1;k++)
-            {
-                if(a[k]>a[k+1])
-                {
-                    temp=a[k];
-                    a[k]=a[k+1];
-                    a[k+1]=temp;
-                }
-
-            }
-        }
+        
         System.out.println("\nSorted Array : ");
         for (int j : a) {
             System.out.print(j+" ");
